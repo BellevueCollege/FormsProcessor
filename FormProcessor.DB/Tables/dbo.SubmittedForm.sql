2fceb49b65e1dc6ae9c52df1fe25a2d5ee7cd980
git <shawn.south@bellevuecollege.edu>
2012-04-12 17:28:41 -0700

    Initial checkin of database using RedGate's SQL Source Control.

---
+++ FormProcessor.DB/Tables/dbo.SubmittedForm.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[SubmittedForm]
+(
+[ID] [uniqueidentifier] NOT NULL,
+[FormID] [uniqueidentifier] NOT NULL,
+[Referrer] [nvarchar] (1024) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Datetime] [datetime] NOT NULL,
+[ClientIP] [nvarchar] (40) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Data] [xml] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[SubmittedForm] ADD CONSTRAINT [PK_SubmittedForm_1] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
+GO
+GRANT INSERT ON  [dbo].[SubmittedForm] TO [CAMPUS\webapp-formpro]
+GO
